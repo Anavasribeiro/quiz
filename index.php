e37a4fb3976fe519effd84856978dd3d2f2194b7
Anna Clara <annaclaragoliveir08@gmail.com>
2024-08-29 15:38:27 -0300

    atualizado

--- index.php
+++ index.php
@@ -4,23 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INDEX</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Quiz</title>
+    <link rel="stylesheet" href="estilo.css"></script>
+
 </head>
 <body>
-<br>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Quiz </title>
-    <script src="https://kit.fontawesome.com/ebda61e3fa.js" crossorigin="anonymous"></script>
-</head>
-<h1>LISTAGEM</h1>
+<h1>PERGUNTAS</h1>
 
 <body>
 <?php
@@ -40,6 +29,7 @@ $perguntaController = new PerguntaController($pdo);
 $perguntaController->exibirListaPergunta();
 //$jogoController->exibirListaJogo();
 //$resultadoController->exibirListaResultados();
+
 
 ?>
 
